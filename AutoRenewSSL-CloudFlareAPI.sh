22b282d0e54f859fc4cc050cea48d8533b0c3b14
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2022-12-05 21:33:20 -0500

    Re-did the script in a lot of parts
    - Changed the way it installs utilities
    - Changed the way it does the cronjobs
    - Added comments

--- AutoRenewSSL-CloudFlareAPI.sh
+++ AutoRenewSSL-CloudFlareAPI.sh
@@ -6,6 +6,7 @@ PRPL=$'\033[1;35m' # Purple
 GRN=$'\e[1;32m' # Green
 BLUE=$'\e[3;49;34m' # Blue
 
+#script logo with copyrights
 printf "${BLUE}\n"
 echo '    ___         __        ____                          __________ __ '
 echo '   /   | __  __/ /_____  / __ \___  ____  ___ _      __/ ___/ ___// / '
@@ -14,12 +15,14 @@ echo ' / ___ / /_/ / /_/ /_/ / _, _/  __/ / / /  __/ |/ |/ /___/ /__/ / /___'
 echo '/_/  |_\__,_/\__/\____/_/ |_|\___/_/ /_/\___/|__/|__//____/____/_____/'
 printf "\nPowered by KeepSec Technologies Inc.™${NC}\n\n"
 
+#check if root or not
 if [ `id -u` -ne 0 ]; then
       printf "${RED}\nThis script can only be executed as root\n\n\n${NC}"
       sleep 0.5
       exit
    fi
 
+#function for the installing wheel
 function installing {
   tput civis
   spinner="⣾⣽⣻⢿⡿⣟⣯⣷"
@@ -32,31 +35,48 @@ function installing {
   done
 }
 
+#executes function above
 installing &
 SPIN_PID=$!
 disown
 printf "${PRPL}\nInstalling utilities ➜ ${NC}"
 
+
+#checks package manager and then install all the necessary utilities with your right package manager
 if [ -n "$(command -v apt-get)" ]; then
   add-apt-repository ppa:certbot/certbot &> /dev/null
-  apt-get -y install pip python3 certbot &> /dev/null
-  pip3 install certbot-nginx &> /dev/null
-  pip3 install certbot-apache&> /dev/null
-  pip3 install certbot-dns-cloudflare &> /dev/null
+  apt-get -y install pip python3 &> /dev/null
+  apt remove -y certbot &> /dev/null
+  snap remove certbot &> /dev/null
+  python3 -m venv /opt/certbot/ &> /dev/null
+  yes | /opt/certbot/bin/pip install --upgrade pip &> /dev/null
+  yes | /opt/certbot/bin/pip3 install certbot &> /dev/null
+  yes | /opt/certbot/bin/pip3 install certbot-nginx &> /dev/null
+  yes | /opt/certbot/bin/pip3 install certbot-apache &> /dev/null
+  yes | /opt/certbot/bin/pip3 install certbot-dns-cloudflare &> /dev/null
+  ln -s /opt/certbot/bin/certbot /usr/bin/certbot &> /dev/null
 elif [ -n "$(command -v yum)" ]; then
   yum -y install epel-release &> /dev/null
-  yum -y install python3 certbot &> /dev/null
-  pip3 install certbot-nginx &> /dev/null
-  pip3 install certbot-apache &> /dev/null
-  pip3 install certbot-dns-cloudflare &> /dev/null
+  yum -y install python3 &> /dev/null
+  yum -y remove certbot &> /dev/null
+  snap remove certbot &> /dev/null
+  python3 -m venv /opt/certbot/ &> /dev/null
+  yes | /opt/certbot/bin/pip install --upgrade pip &> /dev/null
+  yes | /opt/certbot/bin/pip3 install certbot &> /dev/null
+  yes | /opt/certbot/bin/pip3 install certbot-nginx &> /dev/null
+  yes | /opt/certbot/bin/pip3 install certbot-apache &> /dev/null
+  yes | /opt/certbot/bin/pip3 install certbot-dns-cloudflare &> /dev/null
+  ln -s /opt/certbot/bin/certbot /usr/bin/certbot &> /dev/null
 fi
 
+#kills spinning wheel
 kill -9 $SPIN_PID &>/dev/null
 tput cnorm
 echo ""
 echo ""
 echo ""
 
+#series of questions to get your parameters
 read -p "What is the ${YEL}domain${NC} that you want to deploy a wildcard certificate for : " domain
 echo ""
 sleep 0.5
@@ -64,6 +84,7 @@ read -p "What is your ${YEL}email${NC} that you want to use for updates on your 
 echo ""
 sleep 0.5
 
+#function to make your web server only nginx or apache
 function webserver {
 read -p "Prefered web server between ${YEL}nginx${NC} and ${YEL}apache${NC} : " websrv
 if [[ "${websrv}" != @(nginx|apache) ]]; then
@@ -71,12 +92,30 @@ if [[ "${websrv}" != @(nginx|apache) ]]; then
     sleep 0.5
     webserver
 fi
+#adjust between 'apache2' or 'httpd' between os if you choose apache for the restart command at the end
+if [[ "${websrv}" = "apache" ]]; then
+      if [ -n "$(command -v apt-get)" ]; then
+        websrvDef="apache2"
+        restartcmd="/etc/init.d/$websrvDef restart"
+      elif [ -n "$(command -v yum)" ]; then
+        websrvDef="httpd"
+        restartcmd="systemctl restart $websrvDef"
+      fi
+elif [[ "${websrv}" = "nginx" ]]; then
+    websrvDef="nginx"
+      if [ -n "$(command -v apt-get)" ]; then
+        restartcmd="/etc/init.d/$websrvDef restart"
+      elif [ -n "$(command -v yum)" ]; then
+        restartcmd="systemctl restart $websrvDef"
+      fi
+fi
 }
 
 webserver
 
 echo ""
 sleep 0.5
+#asks for CloudFlare API Token
 read -p "What is your ${YEL}CloudFlare API Token${NC} : " token
 echo -e "\n(If you don't know what that is go to ${GRN}https://developers.cloudflare.com/fundamentals/api/get-started/create-token)${NC}"
 sleep 1.5
@@ -84,8 +123,10 @@ echo ""
 echo -e "Starting Certbot...\n"
 sleep 0.5
 
-certbot certonly --agree-tos --email $email --$websrv --preferred-challenges=dns -d *.$domain --server https://acme-v02.api.letsencrypt.org/directory
+#execute certbot command
+(certbot certonly --agree-tos --email $email --$websrv --preferred-challenges=dns -d *.$domain --server https://acme-v02.api.letsencrypt.org/directory --force-renewal) &> /dev/null
 
+#stores api token to auto renew for future occasions
 mkdir /etc/letsencrypt/.certbot/ &> /dev/null
 mkdir /etc/letsencrypt/.certbot/.secret/ &> /dev/null
 tee -a /etc/letsencrypt/.certbot/.secret/cloudflare.$domain.ini > /dev/null <<EOT
@@ -94,11 +135,17 @@ dns_cloudflare_api_token = ${token}
 EOT
 chmod 600 /etc/letsencrypt/.certbot/.secret/cloudflare.$domain.ini &> /dev/null
 
-croncmd="root /usr/bin/certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d *.${domain} --force-renewal >> /var/log/certbot-cloudflare-api.log"
-cronjob="0 0 2 * * $croncmd"
+#makes cronjob to execute certbot every 2 month (lets encrypt needs to be renewed every 3 months), also outputs execution in /var/log/certbot-cloudflare-api.log when it runs
+croncmd1="root /usr/bin/certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d *.${domain} --force-renewal >> /var/log/certbot-cloudflare-api.log"
+cronjob1="0 0 2 * * $croncmd1"
+#also restart your web server when the certbot cronjob executes
+croncmd2="root /usr/bin/bash $restartcmd"
+cronjob2="0 0 2 * * $croncmd2"
 
-printf "$cronjob\n" > /etc/cron.d/$domain-wild-SSL
+#puts the cronjob in /etc/cron.d/
+printf "$cronjob1\n$cronjob2\n" > /etc/cron.d/$domain-wild-SSL
 
+#bye bye message :)
 printf "${GRN}We're done!\n\n${NC}"
 
 exit
