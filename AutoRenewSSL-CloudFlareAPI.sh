2c750819dc8e9a971339981c1a8d65cb8d91dd62
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2023-05-05 00:22:18 -0400

    Update AutoRenewSSL-CloudFlareAPI.sh

--- AutoRenewSSL-CloudFlareAPI.sh
+++ AutoRenewSSL-CloudFlareAPI.sh
@@ -5,6 +5,8 @@ NC=$'\033[0m' # No Color
 PRPL=$'\033[1;35m' # Purple
 GRN=$'\e[1;32m' # Green
 BLUE=$'\e[3;49;34m' # Blue
+dqt='"'
+sqt="'"
 
 #script logo with copyrights
 printf "${BLUE}\n"
@@ -136,8 +138,6 @@ chmod 600 /etc/letsencrypt/.certbot/.secret/cloudflare.$domain.ini &> /dev/null
 (certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d ${sqt}*.${domain}${sqt} --non-interactive --force-renewal) >> /var/log/certbot-cloudflare-api.log
 
 #makes cronjob to execute certbot every 2 month (lets encrypt needs to be renewed every 3 months), also outputs execution in /var/log/certbot-cloudflare-api.log when it runs
-dqt='"'
-sqt="'"
 croncmd1="root /bin/bash -c ${dqt}U | /usr/bin/certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d ${sqt}*.${domain}${sqt} --non-interactive --force-renewal >> /var/log/certbot-cloudflare-api.log${dqt}"
 cronjob1="0 0 2 * * $croncmd1"
 #also restart your web server when the certbot cronjob executes
