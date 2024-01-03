094435059298d4c4c1070a7ca77010386f0984b0
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2024-01-03 12:28:57 -0500

    Update AutoRenewSSL-CloudFlareAPI.sh

--- AutoRenewSSL-CloudFlareAPI.sh
+++ AutoRenewSSL-CloudFlareAPI.sh
@@ -129,10 +129,30 @@ dns_cloudflare_api_token = ${token}
 EOT
 chmod 600 /etc/letsencrypt/.certbot/.secret/cloudflare.$domain.ini &> /dev/null
 
+#put rename script in /etc/letsencrypt/.certbot/rename-LE-dir.sh
+cat >"/etc/letsencrypt/.certbot/rename-LE-dir.sh" <<EOF
+#!/bin/bash
+
+# Define the base directory
+base_dir="/etc/letsencrypt/live"
+
+# List, sort, and get the highest numbered directory
+highest_dir=$(ls -dv ${base_dir}/${1}-* 2>/dev/null | tail -n 1)
+
+# Check if the highest numbered directory exists
+if [ -n "$highest_dir" ]; then
+    # Rename the highest numbered directory
+    mv "$highest_dir" "${base_dir}/${1}"
+    echo "Renamed $highest_dir to ${base_dir}/vpsmini.keepsec.cloud"
+else
+    echo "No directories found to rename."
+fi
+EOF
+
 #makes cronjob to execute certbot every week (lets encrypt needs to be renewed every 3 months), also outputs execution in /var/log/certbot-cloudflare-api.log when it runs
 dqt='"'
 sqt="'"
-croncmd1="root /bin/bash -c ${dqt}/usr/bin/certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d ${sqt}*.${domain}${sqt} --cert-name "${domain}"  --non-interactive --force-renewal >> /var/log/certbot-cloudflare-api.log${dqt} && mv $(ls -d /etc/letsencrypt/live/${domain}-*/ | sed 's/[^0-9]*//g' | sort -n | tail -1 | xargs -I {} echo "/etc/letsencrypt/live/${domain}-{}") /etc/letsencrypt/live/${domain} && systemctl restart nginx"
+croncmd1="root /bin/bash -c ${dqt}/usr/bin/certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d ${sqt}*.${domain}${sqt} --cert-name "${domain}"  --non-interactive --force-renewal >> /var/log/certbot-cloudflare-api.log${dqt} && bash /etc/letsencrypt/.certbot/rename-LE-dir.sh ${domain} && systemctl restart nginx"
 cronjob1="0 0 * * 0 $croncmd1"
 
 #execute the first renewal
