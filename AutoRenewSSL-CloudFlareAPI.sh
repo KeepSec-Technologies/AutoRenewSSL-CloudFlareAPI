4de37f0730456f6ef3c76e29679f99c13c2ed4b8
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2025-02-14 13:42:05 -0500

    Update AutoRenewSSL-CloudFlareAPI.sh

--- AutoRenewSSL-CloudFlareAPI.sh
+++ AutoRenewSSL-CloudFlareAPI.sh
@@ -54,10 +54,10 @@ if [ -n "$(command -v apt-get)" ]; then
   yes | /opt/certbot/bin/pip3 install certbot-apache &>/dev/null
   yes | /opt/certbot/bin/pip3 install certbot-dns-cloudflare &>/dev/null
   ln -s /opt/certbot/bin/certbot /usr/bin/certbot &>/dev/null
-elif [ -n "$(command -v yum)" ]; then
-  yum -y install epel-release &>/dev/null
-  yum -y install python39 &>/dev/null
-  yum -y remove certbot &>/dev/null
+elif [ -n "$(command -v dnf)" ]; then
+  dnf -y install epel-release &>/dev/null
+  dnf -y install python39 &>/dev/null
+  dnf -y remove certbot &>/dev/null
   snap remove certbot &>/dev/null
   python3 -m venv /opt/certbot/ &>/dev/null
   yes | /opt/certbot/bin/pip install --upgrade pip &>/dev/null
@@ -73,79 +73,112 @@ kill -9 $SPIN_PID &>/dev/null
 tput cnorm
 echo ""
 echo ""
-echo ""
 
 # Check for successful package installation
 if ! command -v certbot &>/dev/null; then
-  printf "${RED}Certbot installation failed! Please check the logs.${NC}\n"
+  printf "${RED}\nCertbot installation failed! Please check the logs.${NC}\n"
+  sleep 0.5
   exit 1
 fi
 
-#series of questions to get your parameters
-read -p "What is the ${YEL}domain${NC} that you want to deploy a wildcard certificate for : " domain
-echo ""
-sleep 0.5
-read -p "What is your ${YEL}email${NC} that you want to use for updates on your certificate renew state : " email
-echo ""
-sleep 0.5
+# function to get your domain
+function certbot_domain {
+  echo ""
+  read -p "What is the ${YEL}domain${NC} that you want to deploy a wildcard certificate for: " domain
+  domain=$(echo $domain | tr '[:upper:]' '[:lower:]')
+  # check if the domain is valid format
+  if [[ ! "$domain" =~ ^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+    printf "${RED}\nInvalid domain format! Please try again.\n${NC}"
+    sleep 0.5
+    certbot_domain
+  fi
+  sleep 0.5
+}
 
-#function to make your web server only nginx or apache
+# function to get your email
+function certbot_email {
+  echo ""
+  read -p "What is your ${YEL}email${NC} address for certbot: " email
+  email=$(echo $email | tr '[:upper:]' '[:lower:]')
+  # check if the email is valid format
+  if [[ ! "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+    printf "${RED}\nInvalid email format! Please try again.\n${NC}"
+    sleep 0.5
+    certbot_email
+  fi
+  sleep 0.5
+}
+
+# function to make your web server only nginx or apache
 function webserver {
-  read -p "Prefered web server between ${YEL}nginx${NC} and ${YEL}apache${NC} : " websrv
+  echo ""
+  read -p "What is used between ${YEL}nginx${NC} and ${YEL}apache${NC}: " websrv
+  # make it lower case
+  websrv=$(echo $websrv | tr '[:upper:]' '[:lower:]')
   if [[ "${websrv}" != @(nginx|apache) ]]; then
-    printf "${RED}Type either nginx or apache in lowercase! Try again\n\n${NC}"
+    printf "${RED}\nPlease type either nginx or apache! Try again\n${NC}"
     sleep 0.5
     webserver
   fi
-  #adjust between 'apache2' or 'httpd' between os if you choose apache for the restart command at the end
+
+  # adjust between 'apache' or 'nginx' reload commands
   if [[ "${websrv}" = "apache" ]]; then
-    if [ -n "$(command -v apt-get)" ]; then
-      websrvDef="apache2"
-      restartcmd="/etc/init.d/$websrvDef restart"
-    elif [ -n "$(command -v yum)" ]; then
-      websrvDef="httpd"
-      restartcmd="systemctl restart $websrvDef"
-    fi
+    reloadcmd="apachectl -k graceful"
   elif [[ "${websrv}" = "nginx" ]]; then
-    websrvDef="nginx"
-    if [ -n "$(command -v apt-get)" ]; then
-      restartcmd="/etc/init.d/$websrvDef restart"
-    elif [ -n "$(command -v yum)" ]; then
-      restartcmd="systemctl restart $websrvDef"
-    fi
+    reloadcmd="nginx -s reload"
   fi
+  sleep 0.5
 }
+
+# function to get your CloudFlare API Token
+function cloudflare_token {
+  echo ""
+  echo -e "(If you don't know what that is, go to ${BLUE}https://developers.cloudflare.com/fundamentals/api/get-started/create-token${NC})"
+  read -p "What is your ${YEL}CloudFlare API Token${NC}: " token
+
+  # Verify the CloudFlare API Token
+  if ! curl -s -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" -H "Authorization: Bearer ${token}" | grep -q '"success":true'; then
+    printf "${RED}\nInvalid CloudFlare API Token! Please try again.\n${NC}"
+    sleep 0.5
+    cloudflare_token
+  else 
+    printf "${GRN}\nCloudFlare API Token verified successfully!\n${NC}"
+    sleep 0.5
+  fi
+
+}
+
+certbot_domain
+
+certbot_email
 
 webserver
 
-sleep 0.5
-#asks for CloudFlare API Token
-echo -e "\n(If you don't know what that is go to ${GRN}https://developers.cloudflare.com/fundamentals/api/get-started/create-token${NC})"
-read -p "What is your ${YEL}CloudFlare API Token${NC} : " token
-sleep 1.5
+cloudflare_token
+
 echo -e "\nStarting Certbot...\n"
 sleep 0.5
 
-#stores api token to auto renew for future occasions
+# stores api token to auto renew for future occasions
 mkdir -p /etc/letsencrypt/.certbot/.secret/ &>/dev/null
 tee /etc/letsencrypt/.certbot/.secret/cloudflare.$domain.ini >/dev/null <<EOT
-# Cloudflare API token used by Certbot for all domains on mydomain account
+# Cloudflare API token used by Certbot for $domain wildcard SSL renewal
 dns_cloudflare_api_token = ${token}
 EOT
 chmod 600 /etc/letsencrypt/.certbot/.secret/cloudflare.$domain.ini &>/dev/null
 chown root:root /etc/letsencrypt/.certbot/.secret/cloudflare.$domain.ini &>/dev/null
 
-#makes cronjob to execute certbot every day
-cronjob="0 0 * * root /bin/bash -c '/usr/bin/certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d \"*.${domain}\" --non-interactive --deploy-hook \"nginx -s reload\" >> /var/log/certbot-cloudflare-api.log 2>&1'"
+# makes cronjob to execute certbot every day
+cronjob="0 0 * * root /bin/bash -c '/usr/bin/certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d \"*.${domain}\" --non-interactive --deploy-hook \"$reloadcmd\" >> /var/log/certbot-cloudflare-api.log 2>&1'"
 
-#execute the first renewal
-certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d "*.${domain}" --cert-name "${domain}" --non-interactive --agree-tos --email ${email} --force-renewal >> /var/log/certbot-cloudflare-api.log
-$restartcmd
+# execute the first renewal
+certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/.certbot/.secret/cloudflare.${domain}.ini --preferred-challenges dns -d "*.${domain}" --cert-name "${domain}" --non-interactive --agree-tos --email ${email} --force-renewal > /var/log/certbot-cloudflare-api.log
+$reloadcmd
 
-#puts the cronjob in /etc/cron.d/
+# puts the cronjob in /etc/cron.d/
 printf "$cronjob\n\n" > /etc/cron.d/$domain-wild-SSL
 
-#bye bye message :)
+# bye bye message :)
 printf "${GRN}\nWe're done!\n\n${NC}"
 
 exit
